66095a0c96fdd21eaa5be0df74fdd09f3807dfc4
Sid <sid.dara18@gmail.com>
2020-12-06 14:48:35 -0500

    Added a Database with the help of Firebase by google

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
 var ball;
+var database, position;
 
 function setup(){
     createCanvas(500,500);
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+
+    database = firebase.database();
+    var ballPosition = database.ref("Ball/Position");
+    ballPosition.on("value", readPosition, showError);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+function writePosition(x,y){
+    database.ref("Ball/Position").set({
+        x: position.x + x, y: position.y + y
+    })
+}
+
+function readPosition(data){
+    position = data.val()
+    ball.x = position.x
+    ball.y = position.y
+}
+
+function showError(){
+    console.log("error has occurred")
 }
